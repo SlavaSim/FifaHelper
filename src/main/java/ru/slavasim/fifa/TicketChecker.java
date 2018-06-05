7efb9b739d98374bcd71ac1c076d363689d99750
SlavaSim <slava.sim@gmail.com>
2018-06-06 00:26:48 +0300

    alert on new matches

--- src/main/java/ru/slavasim/fifa/TicketChecker.java
+++ src/main/java/ru/slavasim/fifa/TicketChecker.java
@@ -9,13 +9,15 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.DefaultProxyRoutePlanner;
 import org.springframework.web.client.RestTemplate;
 import ru.slavasim.fifa.model.Availability;
+import ru.slavasim.fifa.model.AvailabilityData;
 
 import java.net.URI;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class TicketChecker/* extends TelegramLongPollingBot*/ implements Runnable {
     public static final String avURL = "https://tickets.fifa.com/API/WCachedL1/en/Availability/GetAvailability";
@@ -27,6 +29,7 @@ public class TicketChecker/* extends TelegramLongPollingBot*/ implements Runnabl
     private int[] categories;
     private LocalDateTime lastCachedTime = null;
     private final String USER_AGENT = "Mozilla/5.0";
+    private Set<String> lastMatchSet = new HashSet<>();
 
     public TicketChecker(String botToken, String chatId, String proxyAddress, int proxyPort, int[] categories) {
         this.botToken = botToken;
@@ -51,19 +54,41 @@ public class TicketChecker/* extends TelegramLongPollingBot*/ implements Runnabl
     }
 
     private void processAvailability(Availability availability) {
-        boolean found = availability.getData().stream().anyMatch(d -> Arrays.binarySearch(categories, d.c) >= 0 && d.a == 1);
-        if (found) {
-            sendAlert(mainChatId);
-            sendAlert("561688131");
+        List<AvailabilityData> freshData = availability.getData();
+        Set<String> freshSet = freshData.stream()
+                .filter(d -> Arrays.binarySearch(categories, d.c) >= 0 && d.a == 1)
+                .map(d -> d.p)
+                .collect(Collectors.toSet());
+        if(!lastMatchSet.containsAll(freshSet)){
+            HashSet<String> diff = new HashSet<>(freshSet);
+            diff.removeAll(lastMatchSet);
+            lastMatchSet = freshSet;
+            if (!diff.isEmpty()){
+                StringBuilder builder = new StringBuilder("Появились новые билеты:");
+                for (String match : diff) {
+                    builder.append("\n").append(match);
+                }
+                String message = builder.toString();
+                sendAlert(mainChatId, message);
+                sendAlert("561688131", message);
+            }
         }
+/*
+
+        boolean found = freshData.stream().anyMatch(d -> Arrays.binarySearch(categories, d.c) >= 0 && d.a == 1);
+        if (found) {
+            sendAlert(mainChatId, message);
+            sendAlert("561688131", message);
+        }
+*/
     }
 
-    private void sendAlert(String chatId) {
+    private void sendAlert(String chatId, String message) {
         try {
 //            String url = TELEGRAM_BOT_URL + botToken + "/sendMessage?chat_id=" + chatId + "&text=Tickets";
             URI uri = new URIBuilder(TELEGRAM_BOT_URL + botToken + "/sendMessage")
                     .setParameter("chat_id", chatId)
-                    .setParameter("text", "Есть билеты!")
+                    .setParameter("text", message)
                     .build();
 
             HttpHost proxy = new HttpHost(proxyAddress, proxyPort);
